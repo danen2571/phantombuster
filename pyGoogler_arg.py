17bee9e4f9da8aa7ce420e558d0d6fd2733584cb
danen2571 <root@localhost.localdomain>
2020-05-26 05:07:21 +0200

    260520

--- pyGoogler_arg.py
+++ pyGoogler_arg.py
@@ -32,39 +32,39 @@ with open(sys.argv[1], 'r') as stringSource:
     lines = []
     for line in stringSource:
         queryInput = re.sub(r'[\n\r\t]*', '', line)
+        print("Fetching results for string " + queryInput)
         csvFilename = queryInput.replace(' ', '_') + '.csv'
         while True:
             try:
                 for url in search(queryInput,        # The query you want to run
-        #                    tld = 'com',  # The top level domain
-        #                    lang = 'en',  # The language
-        #                    start = 0,    # First result to retrieve
-        #                    stop = 20,  # Last result to retrieve
+#                           tld = 'com',  # The top level domain
+#                           lang = 'en',  # The language
+#                           start = 0,    # First result to retrieve
+#                           stop = 20,  # Last result to retrieve
                             num = 10,     # Number of results per page
                             pause = 5.0,  # Lapse between HTTP requests
                            ):
                     result = []
-
-                title = re.sub(r'[\n\r\t]*', '', scrapeTitle(url))
-                if 'ERROR:' in title:
-                    errorMsg = title
-                    title = ""
-                    result = ([i, title, url, errorMsg])
-                else:
-                    result = ([i, title, url])
-        
-                result_list.append(result)
-                data = [result]
-                wr = open('RESULTS/' + csvFilename, 'a', newline ='')
-                with wr:
-                    write = csv.writer(wr)
-                    write.writerows(data)
-                print(result)
-                print(" ")
-                i += 1
-                break
+                    title = re.sub(r'[\n\r\t]*', '', scrapeTitle(url))
+                    if 'ERROR:' in title:
+                        errorMsg = title
+                        title = ""
+                        result = ([url, title, i, errorMsg])
+                    else:
+                        result = ([url, title, i])
+            
+                    result_list.append(result)
+                    data = [result]
+                    wr = open('RESULTS/' + csvFilename, 'a', newline ='')
+                    with wr:
+                        write = csv.writer(wr)
+                        write.writerows(data)
+                    print(result)
+                    print(" ")
+                    i += 1
+#                    break
             except Exception as error:
                 if "Too Many Requests" in str(error):
                     print('Too many requests, sleeping for 5 minutes...')
-                    time.sleep(300.0);
+                    time.sleep(300.0)
                     pass
